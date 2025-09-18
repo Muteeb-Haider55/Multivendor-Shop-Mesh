e8258873398717ac9846378babe93de5b60d6e26
Muteeb-Haider55 <b-27240@student.usa.edu.pk>
2025-09-18 08:21:35 -0700

    chore: manage routes for users && sellers

---
+++ frontend/src/routes/ShopRoutes.js
@@ -0,0 +1,15 @@
+import ShopHomePage from "../pages/Shop/ShopHomePage.jsx";
+import ShopDashboardPage from "../pages/Shop/ShopDashboardPage.jsx";
+import ShopCreateProduct from "../pages/Shop/ShopCreateProduct.jsx";
+import ShopAllProduct from "../pages/Shop/ShopAllProduct.jsx";
+import ShopCreateEvent from "../pages/Shop/ShopCreateEvent.jsx";
+import ShopAllEvents from "../pages/Shop/ShopAllEvents.jsx";
+
+export {
+  ShopHomePage,
+  ShopCreateEvent,
+  ShopDashboardPage,
+  ShopCreateProduct,
+  ShopAllProduct,
+  ShopAllEvents,
+};
